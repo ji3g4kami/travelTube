f9a497772d286cdd1c97b22cf5f53e5a152b65ef
Deng Li <ji3g4kami@gmail.com>
2018-04-30 22:28:43 +0800

    youtubePlayer can work

--- travelTube/ArticleCollectionViewCell.swift
+++ travelTube/ArticleCollectionViewCell.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import YouTubePlayer
 
 class ArticleCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var videoPlayer: YouTubePlayerView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- travelTube/CategoryCell.swift
+++ travelTube/CategoryCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class CategoryCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var articleCollectionView: UICollectionView!
-    var imageArray: [UIImage] = [#imageLiteral(resourceName: "united-states"), #imageLiteral(resourceName: "united-kingdom"), #imageLiteral(resourceName: "france"), #imageLiteral(resourceName: "germany"), #imageLiteral(resourceName: "china")]
+    let taipeiVideo: [String] = ["R_QzFdQp4_4", "kPFQCpzLTME", "CRq3hOFqlgI"]
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -33,19 +33,19 @@ class CategoryCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewD
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
+        return taipeiVideo.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell: ArticleCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "articleCell", for: indexPath) as? ArticleCollectionViewCell{
-            cell.imageView.image = imageArray[indexPath.row]
+            cell.videoPlayer.loadVideoID(taipeiVideo[indexPath.row])
             return cell
         }
         return UICollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
-        let size = CGSize(width: 120, height: 120)
+        let size = CGSize(width: 150, height: 120)
         return size
     }
     
