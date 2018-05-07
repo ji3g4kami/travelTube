756b5c5be986e2fc1e934d8358d8241d27e694c5
Deng Li <ji3g4kami@gmail.com>
2018-05-07 23:19:27 +0800

    google sign in

---
+++ travelTube/View/Login/GoogleLoginButton.swift
@@ -0,0 +1,19 @@
+//
+//  GoogleLoginButton.swift
+//  travelTube
+//
+//  Created by 吳登秝 on 2018/5/7.
+//  Copyright © 2018年 DavidWu. All rights reserved.
+//
+
+import UIKit
+
+class GoogleLoginButton: UIButton {
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = 5.0
+    }
+}
