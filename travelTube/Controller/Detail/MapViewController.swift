bb814b3542cf2cf0f666816f7edc93c72f43a113
Deng Li <ji3g4kami@gmail.com>
2018-05-18 12:14:58 +0800

    full screen map created

---
+++ travelTube/Controller/Detail/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  travelTube
+//
+//  Created by 吳登秝 on 2018/5/18.
+//  Copyright © 2018年 DavidWu. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
