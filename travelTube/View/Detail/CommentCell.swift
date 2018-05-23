816b1498ff843a4b0aa0d19c2d7660b093dcb39d
Deng Li <ji3g4kami@gmail.com>
2018-05-23 10:45:19 +0800

    set userImage in comments to circular image

--- travelTube/View/Detail/CommentCell.swift
+++ travelTube/View/Detail/CommentCell.swift
@@ -16,12 +16,11 @@ class CommentCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        userProfileImage.setRounded()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
 
