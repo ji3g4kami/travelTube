f348432d098a78e3e80c3b0e0995ba89ba9e569c
Deng Li <ji3g4kami@gmail.com>
2018-05-31 16:22:16 +0800

    edit page can show all annotations

--- travelTube/Controller/Detail/EditArticleViewController.swift
+++ travelTube/Controller/Detail/EditArticleViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class EditArticleViewController: UIViewController {
 
     var articleInfo: Article?
+    var annotations = [MKPointAnnotation]()
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(articleInfo)
+        setupMap()
+    }
+
+    private func setupMap() {
+        guard let annotaions = articleInfo?.annotations else { return }
+        for annotaion in annotaions {
+            let marker = MKPointAnnotation()
+            marker.title = annotaion.title
+            marker.coordinate = CLLocationCoordinate2DMake(annotaion.latitude, annotaion.logitutde)
+            self.annotations.append(marker)
+        }
+        mapView.addAnnotations(self.annotations)
+        mapView.showAnnotations(mapView.annotations, animated: true)
+
+        // resize rect to make it look better MKMapRect
+        let resizedSpan = MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta*1.1, longitudeDelta: mapView.region.span.longitudeDelta*1.1)
+        let resizedRegion = MKCoordinateRegion(center: mapView.region.center, span: resizedSpan)
+        mapView.setRegion(resizedRegion, animated: true)
     }
 
     @IBAction func cancelButtonPressed(_ sender: Any) {
